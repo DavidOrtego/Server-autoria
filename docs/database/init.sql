2c283796b21d9d7ce5d1abf38e52e537d5f2d488
DavidOrtego <davidortego2016@gmail.com>
2026-04-27 16:17:29 +0200

    feat: creada la tabla de usuarios

--- docs/database/init.sql
+++ docs/database/init.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE IF NOT EXISTS vives_db;
 USE vives_db;
 
+-- 1. TABLA: Usuarios
+CREATE TABLE IF NOT EXISTS Usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    contraseña VARCHAR(255) NOT NULL,
+    rol ENUM('admin', 'miembro') DEFAULT 'miembro',
+    foto VARCHAR(255)

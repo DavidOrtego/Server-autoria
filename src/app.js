1df51bc942e5e379eb721f255281d24e97339214
DavidOrtego <davidortego2016@gmail.com>
2026-04-27 19:59:19 +0200

    feat: initialize Express server with CORS, JSON middleware, and ping endpoint

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+// const usuariosRoutes = require('./routes/usuarios'); cuando se añada routes/usurios quitar comentarios
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+// app.use('/api/usuarios', usuariosRoutes);
+
+app.get('/api/ping', (req, res) => {
+  res.json({ mensaje: '¡Pong! El servidor de Vives está vivo.' });
+});
+
+module.exports = app;

3fdf0623e3ccc6f4e8e31f434807d812c08c49da
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 23:02:30 +0200

    feat: middlerware para verificar su el usuario es admin

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -27,3 +27,32 @@ const authenticateToken = (req, res, next) => {
     });
   }
 };
+
+/**
+ * Middleware para verificar si el usuario es administrador
+ * IMPORTANTE: Este middleware siempre debe ponerse en la ruta DESPUÉS de authenticateToken
+ */
+const isAdmin = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({
+      success: false,
+      message: "No autenticado",
+    });
+  }
+ 
+  if (req.user.rol !== "admin") {
+    return res.status(403).json({
+      success: false,
+      message: "Acceso denegado. Se requieren permisos de administrador.",
+    });
+  }
+
+
+  next();
+};
+
+
+module.exports = {
+  authenticateToken,
+  isAdmin,
+};

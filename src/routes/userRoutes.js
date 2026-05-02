d0ecc15bdd87c392296ba19503d40e41bf68262a
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 23:45:32 +0200

    feat: implementada ruta para obtener todos los usuarios (GET ALL)

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+
+// Importamos el controlador de usuarios
+const userController = require("../controllers/userController");
+
+
+// Importamos los middlewares de seguridad
+const { authenticateToken, isAdmin } = require("../middlewares/authMiddleware");
+
+
+// Importamos validadores y manejador de errores de validación
+const { handleValidationErrors } = require("../middlewares/errorHandler");
+const { userIdValidator, putUserValidator } = require("../validators/userValidator");
+
+
+// Verificar que hay un usuario logueado y guardar los datos en req.user
+router.use(authenticateToken);
+
+
+// RUTAS DE USUARIOS
+
+
+// GET /users -> Obtiene todos los usuarios
+// isAdmin para que solo el usuario administrador pueda acceder
+router.get("/", isAdmin, userController.getAllUsers);

939ba6442146b640ef3706f873ba3e56b5129b0e
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 22:56:15 +0200

    feat: controlador (GET BY ID) para obtener usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,3 +26,31 @@ const getAllUsers = async (req, res, next) => {
     next(error);
   }
 };
+/**
+ * Obtiene un usuario por su ID
+ * @param {Object} req - Objeto de petición de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @param {Function} next - Función middleware para manejo de errores.
+ * @returns {Promise<void>} Devuelve una respuesta JSON con código 200 y los datos
+ */
+const getUserById = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await findUserById(userId);
+    if (!user) {
+      return res.status(404).json({
+        code: 404,
+        title: "Not Found",
+        message: `Usuario con id ${userId} no encontrado`,
+      });
+    }
+    res.status(200).json({
+      code: 200,
+      title: "Success",
+      message: `Usuario con id ${userId} obtenido correctamente`,
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};

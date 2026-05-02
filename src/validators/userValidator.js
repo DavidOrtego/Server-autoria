268ea74302137fe58659391b19ced461bde33ced
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 23:12:18 +0200

    feat: validador de los datos al modificar usuario

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -7,3 +7,46 @@ const userIdValidator = [
         .withMessage("El ID del usuario debe ser un número entero positivo"),
 ];
 
+const putUserValidator = [
+    body("name")
+        .trim()
+        .notEmpty()
+        .withMessage("El nombre no puede estar vacío.")
+        .isLength({ max: 100 })
+        .withMessage("El nombre no puede exceder los 100 caracteres."),
+
+
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("El correo electrónico no puede estar vacío.")
+        .isEmail()
+        .withMessage("Debe ser un correo electrónico válido.")
+        .isLength({ max: 150 })
+        .withMessage("El correo electrónico no puede exceder los 150 caracteres."),
+
+
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("La contraseña no puede estar vacía.")
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe tener al menos 8 caracteres."),
+
+
+    body("rol")
+        .isIn(["admin", "member"])
+        .withMessage("Rol inválido. Debe ser 'admin' o 'member'."),
+       
+    body("image")
+        .optional()
+        .trim()
+        .isURL()
+        .withMessage("La imagen debe ser una URL válida.")
+];
+
+
+module.exports = {
+    userIdValidator,
+    putUserValidator,
+};

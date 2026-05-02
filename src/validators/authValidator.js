2625584cbdc60e37e7571b212f2bf7d0d997c58d
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 23:07:09 +0200

    feat: validador para comprobar los datos de registro

---
+++ src/validators/authValidator.js
@@ -0,0 +1,36 @@
+const {body, param } = require("express-validator");
+const { confirmPassword } = require("../utils/confirmPassword.js");
+
+
+const registerValidators = [
+    body("name")
+        .trim()
+        .notEmpty()
+        .withMessage("El nombre es requerido.")
+        .isLength({ max: 100 })
+        .withMessage("El nombre no puede exceder los 100 caracteres."),
+
+
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("El correo electrónico es requerido.")
+        .isEmail()
+        .withMessage("Debe ser un correo electrónico válido.")
+        .isLength({ max: 150 })
+        .withMessage("El correo electrónico no puede exceder los 150 caracteres."),
+
+
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("La contraseña es requerida.")
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe tener al menos 8 caracteres."),
+
+
+    body("rol")
+        .optional()
+        .isIn(["admin", "member"])
+        .withMessage("Rol inválido"),
+];

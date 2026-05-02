20e49d513e34afc05b5da7da27526714138e0f12
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 23:08:15 +0200

    feat: validador para comprobar datos de login

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -34,3 +34,22 @@ const registerValidators = [
         .isIn(["admin", "member"])
         .withMessage("Rol inválido"),
 ];
+
+const loginValidators = [
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("El correo electrónico es requerido.")
+        .isEmail()
+        .withMessage("Debe ser un correo electrónico válido.")
+        .isLength({ max: 150 })
+        .withMessage("El correo electrónico no puede exceder los 150 caracteres."),
+
+
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("La contraseña es requerida.")
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe tener al menos 8 caracteres."),
+];

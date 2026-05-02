f7d9a1e6a4629109c5030056942303af3078d8df
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 12:57:50 +0200

    feat: implementado metodo para buscar usuarios por id

---
+++ src/services/userService.js
@@ -0,0 +1,21 @@
+const { db } = require("../config/database");
+const { hashPassword } = require("../utils/encryption");
+
+
+/**
+ * Obtiene un usuario por su ID
+ * @param {number} userId - ID del usuario a obtener
+ * @returns {Promise<Object>} Objeto con el usuario
+ */
+const findUserById = async (userId) => {
+  const user = await db("Users")
+    .select("id_user", "name", "email", "rol", "image")
+    .where({ id_user: userId })
+    .first();
+
+
+  if (!user) {
+    throw { status: 404, message: "Usuario no encontrado" };
+  }
+  return user;
+};

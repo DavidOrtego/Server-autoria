03ba32f369606b4a3950066881bd429ed3fc22ad
DavidOrtego <davidortego2016@gmail.com>
2026-05-02 13:06:58 +0200

    feat: implementado controlador para el login de usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,4 +24,27 @@ class AuthController {
     }
   }
   
+  /**
+   * Inicia sesión en un usuario existente.
+   * @param {Object} req - Request object.
+   * @param {Object} res - Response object.
+   * @param {Function} next - Next function.
+   * @returns {Promise<void>}
+   */
+  async loginUser(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const result = await authService.loginUser(email, password);
+
+
+      res.status(200).json({
+        success: true,
+        message: "Login exitoso",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
 };

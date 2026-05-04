352224ef7a41b9162cc2ef18b1047ee08d3475bd
MT22HUGO <hugomartor07@gmail.com>
2026-05-04 19:21:28 +0200

    feat: service mostrar tareas y mostrar por id.

---
+++ src/services/taskService.js
@@ -0,0 +1,40 @@
+const { db } = require("../config/database");
+
+const findAllTasks = async () => {
+    const tasks = await db("Tasks").select(
+        "id_task",
+        "name",
+        "description",
+        "state",
+        "expiration_date",
+        "id_house",
+        "id_user"
+    );
+    return tasks;
+}
+
+const findTaskById = async (taskId) => {
+    const task = await db("Tasks")
+        .select(
+            "id_task",
+            "name",
+            "description",
+            "state",
+            "expiration_date",
+            "id_house",
+            "id_user"
+        )
+        .where({ id_task: taskId })
+        .first();
+
+    if (!task) {
+        throw { status: 404, message: "Tarea no encontrada" };
+    }
+
+    return task;
+}
+
+module.exports = {
+    findAllTasks,
+    findTaskById,
+}

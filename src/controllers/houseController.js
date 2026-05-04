f71ecd2e44d3f816783097c901ee42094d0b8097
MT22HUGO <hugomartor07@gmail.com>
2026-05-04 15:46:46 +0200

    feat: Implementar endpoint para crear casas.

--- src/controllers/houseController.js
+++ src/controllers/houseController.js
@@ -54,3 +54,30 @@ const getHouseById = async (req, res, next) => {
     next(error);
   }
 };
+
+/**
+ * Crea una casa
+ * @param {Object} req - Objeto de petición de Express.
+ * @param {Object} res - Objeto de respuesta de Express.
+ * @param {Function} next - Función middleware para manejo de errores.
+ * @returns {Promise<void>} Devuelve una respuesta JSON con código 200 y los datos
+ */
+const postHouse = async (req, res, next) => {
+  try {
+    const houseData = req.body;
+    const newId = await createHouse(houseData);
+    const newHouse = {
+      id_house: newId,
+      ...houseData,
+    };
+
+    res.status(201).json({
+      code: 201,
+      title: "Created",
+      message: `Casa con id ${newId} creada correctamente`,
+      data: newHouse,
+    });
+  } catch (error) {
+    next(error);
+  }
+};

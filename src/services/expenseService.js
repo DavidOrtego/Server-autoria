48d7a71eb3647d2b0a3d1f9fb5301660877cb707
MT22HUGO <hugomartor07@gmail.com>
2026-05-04 20:09:11 +0200

    feat: service actualizar gasto.

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -37,8 +37,30 @@ const createExpense = async (expenseData) => {
     return id;
 }
 
+const updateExpense = async (expenseId, newExpenseData) => {
+    const existingExpense = await db("Expenses")
+        .where({ id_expense: expenseId })
+        .first();
+    
+    if (!existingExpense) {
+        throw { status: 404, message: "Gasto no encontrado" };
+    }
+
+    const updateData = {};
+    if (newExpenseData.amount) updateData.amount = newExpenseData.amount;
+    if (newExpenseData.description) updateData.description = newExpenseData.description;
+    if (newExpenseData.date) updateData.date = newExpenseData.date;
+    if (newExpenseData.id_user) updateData.id_user = newExpenseData.id_user;
+    if (newExpenseData.id_house) updateData.id_house = newExpenseData.id_house;
+
+    if (Object.keys(updateData).length > 0) {
+        await db("Expenses").where({ id_expense: expenseId }).update(updateData);
+    }
+}
+
 module.exports = {
     findAllExpenses,
     findExpenseById,
     createExpense,
+    updateExpense,
 }

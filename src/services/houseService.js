f0e9d6da7674b1ff6fe5575b2a501063e97e258a
MT22HUGO <hugomartor07@gmail.com>
2026-05-04 15:39:36 +0200

    feat: Implementar metodo para crear registros de casas.

--- src/services/houseService.js
+++ src/services/houseService.js
@@ -30,3 +30,23 @@ const findHouseById = async (houseId) => {
 
     return house;
 }
+
+const createHouse = async (houseData) => {
+    const existingHouse = await db("Houses")
+        .where({ name: houseData.name })
+        .first();
+    if (existingHouse) {
+        throw { status: 400, message: "La casa ya existe" };
+    }
+
+    const [createdHouse] = await db("Houses")
+        .insert(houseData)
+        .returning([
+            "id_house",
+            "name",
+            "address",
+            "number_of_rooms",
+            "image",
+            "level"
+        ]);
+}

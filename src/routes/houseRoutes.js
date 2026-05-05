0275fa43668ba3a01905f9fd3b2efa055077ca3d
MT22HUGO <hugomartor07@gmail.com>
2026-05-05 20:14:58 +0200

    refactor: poder eliminar tu propia casa.

--- src/routes/houseRoutes.js
+++ src/routes/houseRoutes.js
@@ -52,7 +52,6 @@ router.put(
 // DELETE /houses/:houseId -> Elimina una casa
 router.delete(
   "/:houseId",
-  isAdmin,
   houseIdParamValidator,
   handleValidationErrors,
   houseController.deleteAHouse
